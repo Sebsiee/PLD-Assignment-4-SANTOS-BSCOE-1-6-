f32431c0bec40c195d67b20409dbffb50d29b21b
Sebastian <santosbasty4@gmail.com>
2021-11-06 22:26:11 +0800

    Second Commit

--- Apples & Oranges.py
+++ Apples & Oranges.py
@@ -13,6 +13,15 @@ def display():
     getOranges = int(input("How many oranges do you want to buy? "))
     totalAmountF = applesOranges(getApples, getOranges)
     print (f"The total amount is {totalAmountF}.")
+    solution = str(input("Do you want to see the solution? Y or N "))
+    if solution == "Y":
+        print(f"20 x {getApples} + 25 x {getOranges} = {totalAmountF}")
+    elif solution == "N":
+        print("Wow! You already know how it got solved.")
+    else:
+        print("Input not recognized.")
+
 
 introduction()
 display()
+
